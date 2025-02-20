508e4b13b0261dc5dad8f042971b486aaa9a7a03
HaydarCanDurmaz <candrhydr@hotmail.com>
2025-02-20 16:15:12 +0300

    cookiesAccept1

--- src/MercedesPages/AboutMe.java
+++ src/MercedesPages/AboutMe.java
@@ -1,5 +1,6 @@
 package MercedesPages;
 
+import GWD.Tools;
 import GWD.Utily;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,21 +9,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class AboutMe extends Utily {
 
-    public static void main(String[] args) {
-
-        List<WebElement> cookiesAccept =driver.findElements(By.linkText("Tümünü kabul et"));
-        if (cookiesAccept.size() > 0)
-            cookiesAccept.get(0).click();
+    @Test
+    public void Test1 ()
+    {
 
         driver.get("https://www.mercedes-benz.com.tr");
 
 
+        Tools.wait(6);
+        List<WebElement> cookiesAccept = driver.findElements(By.xpath("Tümünü kabul et"));
+        if (cookiesAccept.size() > 0)
+            cookiesAccept.get(0).click();
 
 
+        WebElement aboutMe = driver.findElement(By.linkText("Hakkımızda"));
+        System.out.println("Hakkımızda get" + aboutMe.getText());
 
-        EndPages();
+        //  EndPages();
 
 
     }
